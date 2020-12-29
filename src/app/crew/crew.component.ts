b1094e87708e9c7e1ae7bcdbb22bc130348426d3
Michal Richter <michalrichter@Michals-Air.attlocal.net>
2020-12-29 15:14:00 -0600

    finished work on studio-2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -10,7 +10,7 @@ export class CrewComponent implements OnInit {
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
   ];
 
   constructor() { }
@@ -18,4 +18,25 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  memberBeingEdited: object = null;
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+  
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
+    
+  
 }
